55cd355cc9901cbb7724228ed8298d7c879f96ab
Waters <166139510+Waters-P@users.noreply.github.com>
2024-05-15 12:23:35 +0000

    42.2% (project)

--- init_admin_info.rb
+++ init_admin_info.rb
@@ -1,18 +1,19 @@
 class Admin_info
-
-  attr_reader :account_id 
+ 
   attr_reader :first_name
-  attr_reader :last_name
+
   attr_accessor :email
-  attr_accessor :username
+
+  attr_reader :account_id  
   
 
-  def initialize (first_name, email) # last_name, email, username
-    @account_id = rand(99999)
+  def initialize (first_name, email) 
+   
+    
     @first_name = first_name
-    @last_name = last_name
     @email = email
-    @username = username
+    @account_id = rand(99999)
+    
     
   end  
   
