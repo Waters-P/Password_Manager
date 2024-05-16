c35266be9becbc3f4daaa4ffeafbd90cc69febfa
Waters <166139510+Waters-P@users.noreply.github.com>
2024-05-16 09:35:59 -0500

    test.rb

--- tests/test.rb
+++ tests/test.rb
@@ -2,13 +2,15 @@ require 'minitest/autorun'
 require './manager'
 
 
+
 class TestManager < Minitest::Test
 
     def test_encrypting
 
         user = The_Password_Manager.new
-        #assert_equal 2024, user.get_date, "pass"
-        assert_equal "Encrypted password", user.encrpt_pw, "pass"
+        
+        assert_nil user.add_admin
          
     end
 end
+
